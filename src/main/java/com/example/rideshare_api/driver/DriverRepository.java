d6f1a79693b3b1eaf02faedb625d2144251bcafa
Sanjeev Thanarasa <59352357+sanjeev-thanarasa@users.noreply.github.com>
2025-09-24 09:41:30 +0530

    Create  a new api application

--- src/main/java/com/example/rideshare_api/driver/DriverRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.rideshare_api.driver;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface DriverRepository extends JpaRepository<Driver, Long> { }
--- src/main/java/com/example/rideshare_api/passenger/PassengerRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.rideshare_api.passenger;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> { }
